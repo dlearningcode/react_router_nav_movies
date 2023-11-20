83fff90139fb97d5bdcbd1f629d0722e07e51e1f
Dion GeBorde <diongeborde@gmail.com>
2023-11-19 19:33:06 -0800

    MovieDetails mapped categories, actors

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -7,14 +7,20 @@ export default function MovieDetails () {
 
     const {id, title, director, releaseYear, IMDBrating, categories, actors, storyline} = movie
 
+    const categoryList = categories.map((category) => { return(
+        <div className='details-category'>{category}</div>
+    )})
+
+    const actorsList = actors.join(', ')
+
     return (
         <div>
             <h1 key={id}>{title}</h1>
-            <p>Categories: {categories}</p>
+            <p>{categoryList}</p>
             <p>Release Year: {releaseYear} * IMDB Rating: {IMDBrating}</p>
             <p>Storyline: {storyline}</p>
             <p>Director: {director}</p>
-            <p>Actors: {actors}</p>
+            <p>Actors: {actorsList}</p>
         </div>
     )
 }
