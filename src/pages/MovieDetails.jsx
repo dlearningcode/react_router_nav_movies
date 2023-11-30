9c1a0fee7ee4252e36c1983228ddf4ac4d63c98b
Dion GeBorde <diongeborde@gmail.com>
2023-11-29 20:55:01 -0800

    FontAwesome, MovieDetails layout, CSS

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,10 +1,13 @@
 import {useLocation} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function MovieDetails () {
 
     // Pull the movie from the location state
     const {movie} = useLocation().state;
 
+    // Destructure the movie object so we can use the properties directly
     const {id, title, director, releaseYear, IMDbRating, categories, actors, storyline} = movie
 
     // make categoryList a list of uppercase <div> elements with a space between each
@@ -19,10 +22,11 @@ export default function MovieDetails () {
         <div className='Movie-details'>
             <h1 key={id}>{title}</h1>
             <div className='Details-categories'>{categoryList}</div>
-            <p>Release Year: {releaseYear} ⚪ <i class="fas fa-star    "></i><i class="fa fa-asterisk" aria-hidden="true"></i> IMDb Rating: {IMDbRating}</p>
-            <p>Storyline: {storyline}</p>
-            <p>Director: {director}</p>
-            <p>Actors: {actorsList}</p>
+            <p id='Details-year'><span>Release Year:</span> {releaseYear} </p> 
+            <p id="Details-rating">IMDb Rating: {IMDbRating}<FontAwesomeIcon icon={faStar} size='xs' /></p>
+            <p id='Details-storyline'><span>Storyline:</span> {storyline}</p>
+            <p id='Details-director'><span>Director:</span> {director}</p>
+            <p id='Details-actors'><span>Actors:</span> {actorsList}</p>
         </div>
     )
 }
